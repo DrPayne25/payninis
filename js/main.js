3e025992a563ea58c5303699765369f26d61977e
DrPayne25 <alex.payne1125@gmail.com>
2021-04-12 09:38:48 -0700

    css and jss updated

--- js/main.js
+++ js/main.js
@@ -2,16 +2,15 @@
 var userId = prompt("Welcome to Payninis! What's your name?");
 var day = new Date();
 var currentTime = day.getHours();
-var today = day.getDay();
 var welcome;
 
 //Welcome Prompt
 if (currentTime > 18){
-    welcome = 'Thank you for coming to Payninis! Might we interest you in Dinner?' +userId;
+    welcome = 'Thank you for coming to Payninis! Might we interest you in Dinner? '  +userId;
 }else if (currentTime > 12){
-    welcome = 'Thank you for coming to Payninis! Might we interest you in Lunch?' +userId
+    welcome = 'Thank you for coming to Payninis! Might we interest you in Lunch? '  +userId
 }else if (currentTime > 0){
-    welcome = 'Thank you for coming to Payninis! Might we interest you in Breakfast?' +userId;
+    welcome = 'Thank you for coming to Payninis! Might we interest you in Breakfast? '  +userId;
 }
 
 document.write('<h3>' + welcome + '</h3>');
