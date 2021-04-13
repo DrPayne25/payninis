ba2eb63a179478fe448f897b1ccafa1c9cffb9c9
DrPayne25 <alex.payne1125@gmail.com>
2021-04-13 00:53:01 -0700

    js updated for functions

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
-/*Variable Vinsmoke declares himself.*/
-var userId = prompt("Welcome to Payninis! What's your name?");
-var day = new Date();
-var currentTime = day.getHours();
-var welcome;
+    var userId = prompt("Welcome to Payninis! What's your name?");
+    var day = new Date();
+    var currentTime = day.getHours();
+    var firstTime = confirm("Is this your first time at Payninis")
 
-//Welcome Prompt
-if (currentTime > 18){
-    welcome = 'Thank you for coming to Payninis! Might we interest you in Dinner? '  +userId;
-}else if (currentTime > 12){
-    welcome = 'Thank you for coming to Payninis! Might we interest you in Lunch? '  +userId
-}else if (currentTime > 0){
-    welcome = 'Thank you for coming to Payninis! Might we interest you in Breakfast? '  +userId;
+    
+/*Variable Vinsmoke declares himself.*/
+function welcomeHere() {
+    var greeting = "Welcome to <b>Payninis<b> "
+    var welcome;
+
+    //Welcome Prompt
+    if (currentTime > 18){
+        welcome = greeting+userId+ ' are you looking to join us for Dinner'; 
+    }else if (currentTime > 12){
+        welcome = greeting+userId+' are you looking to join us for Lunch';
+    }else if (currentTime >= 0){
+        welcome = greeting+userId+' are you looking to join us for Breakfast';
+    }
+
+    return('<h1>' + welcome + '</h1>');}
+
+function thankYou() {
+    var thankYouMessage;
+
+    if (firstTime = true){
+        thankYouMessage = 'option 1' + userId;
+    }else if (firstTime = false){
+        thankYouMessage = "option 2" + userId;
+    }
+
+    return'(<h1>' +thankYouMessage + '</h1>'
 }
 
-document.write('<h3>' + welcome + '</h3>');
+
